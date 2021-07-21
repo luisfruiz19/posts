b1e441275ac2637cef5d7db13778a70b42a45857
Luis Fernando <luisfruiz19@gmail.com>
2021-07-21 18:26:38 -0500

    Feature: prueba de mail

---
+++ app/Http/Controllers/MailAPIController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailAPIController extends Controller
+{
+    //
+
+    public function sendMail(Request $request): string
+    {
+        return 'hola';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(
+    function () {
+        Route::get('send-mail',[\App\Http\Controllers\MailAPIController::class,'sendMail']);
+    });
+
